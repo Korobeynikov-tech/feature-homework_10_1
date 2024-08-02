610206cdbb6b2971d8daadf7da8bc2a95d50c8e7
Korobeynikov-tech <korob-54@mail.ru>
2024-08-02 13:40:06 +0300

    Update masks.py
    добавил пробелы

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 def get_mask_card_number(card_number: str) -> str:
     """Маскирует номер карты, оставляя видимыми первые 6 и последние 4 цифры.
 
@@ -10,6 +11,7 @@ def get_mask_card_number(card_number: str) -> str:
         str: Замаскированный номер карты.
     """
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
 
 def get_mask_account(account_number: str) -> str:
     """Маскирует номер счета, оставляя видимыми только последние 4 цифры.
@@ -22,8 +24,8 @@ def get_mask_account(account_number: str) -> str:
     """
     return f"**{account_number[-4:]}"
 
-# Пример использования функций:
 
+# Пример использования функций:
 card_num = "7000792289606361"
 account_num = "73654108430135874305"
 
@@ -43,6 +45,7 @@ def get_date(date_str):
     date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
     # Преобразуем объект datetime в строку формата "ДД.ММ.ГГГГ"
     return date_obj.strftime("%d.%m.%Y")
+
 
 # Примеры использования функции
 print(get_date("2024-03-11T02:26:18.671407"))  # Выведет: 11.03.2024
